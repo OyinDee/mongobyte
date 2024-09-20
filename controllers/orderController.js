a770820b3bb72a459a3a3e107cb485f5bd51053b
Dee <ajalaoyinda@gmail.com>
2024-09-20 09:00:22 +0100

    taking care of orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,4 @@
-const Order = require('../models/Order');
+const Order = require('../models/Orders');
 const Restaurant = require('../models/Restaurants');
 const sendEmail = require('../configs/nodemailer');
 const User = require('../models/User');
