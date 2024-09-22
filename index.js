6363e3b88f00ebba0544ca60980fac096e6eb47b
Dee <ajalaoyinda@gmail.com>
2024-09-22 21:53:09 +0100

    Delivery fee approval

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3000/',  'https://yumbyte.netlify.app', 'https://yumbyte.netlify.app/', 'https://bytego.vercel.app', 'https://bytego.vercel.app/'],
+    origin: ['http://localhost:3000', 'http://localhost:3000/',  'https://yumbyte.netlify.app', 'https://yumbyte.netlify.app/', 'https://bytego.vercel.app', 'https://bytego.vercel.app/', 'http://192.168.186.74:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
