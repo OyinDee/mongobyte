8f6b91be870c503f06c8825f43495531645fc768
Dee <ajalaoyinda@gmail.com>
2024-09-25 09:33:40 +0100

    added vercel.json

--- index.js
+++ index.js
@@ -44,4 +44,4 @@ mongoose.connect(`${process.env.MONGODB_URI}`, {})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports = { io };
+module.exports = server;
