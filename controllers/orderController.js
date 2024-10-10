3f421640e24626fe3aae3618298f331c6fb6febc
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-10-10 21:37:21 +0100

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -110,7 +110,6 @@ exports.createOrder = async (request, response) => {
             <div class="content">
               <p>You have received a new order.</p>
               <p><strong>Order Details:</strong></p>
-              <p>Total Price: ${totalPrice} naira</p>
               <p>Location: ${location}, around ${nearestLandmark || '...'}</p>
               <p>Phone: ${phoneNumber}</p>
               <p>Please check the dashboard for meal details.</p>
